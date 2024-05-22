29f3f19e2771575ca6dab5ef6d4e2924eba9f3d1
Cinthia Soares De Jesus <00001124680408sp@aluno.educacao.sp.gov.br>
2024-05-22 17:41:31 -0300

    primeiro commit

---
+++ Exercicio04.py
@@ -0,0 +1,6 @@
+nota_final = float(input("Digite a nota final: "))
+if nota_final >= 7:
+    print("Parabens! Voce foi aprovado.")
+else:
+    print("Infelizmente voce nao foi aprovado. Estude mais.")
+
